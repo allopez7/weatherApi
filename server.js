03af8551fd6ade3ebaf2da4690a7888a887b6366
Alberto Lopez <losdodgerz@gmail.com>
2020-12-16 18:52:42 -0800

    Weather Api to display weather in your city

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.post("/weather", async (req, res) => {
+  try {
+    const weatherApi = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${process.env.WEATHER_API_KEY}`
+    );
+    const weatherApiParse = await weatherApi.json();
+    console.log(weatherApiParse)
+    res.json({ express: weatherApiParse });
+  } catch (err) {
+    res.json({ express: weatherApiParse });
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Weather from "./Components/Weather";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState({});
+  const cityInput = (e) => {
+    setCity(e.target.value);
+  };
+  const getWeather = () => {
+    fetch("/weather", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ city: city }),
+    })
+      .then((r) => r.json())
+      .then((weatherData) => {
+        setWeatherInfo(weatherData.express);
+      })
+      .catch((err) => {
+        setWeatherInfo(err.express);
+      });
+  };
+  const { cod, message } = weatherInfo;
+  const errorMessage404 = <div className="error-message">{message}</div>;
+  const errorMessage400 = (
+    <div className="error-message">Enter a city name</div>
+  );
+  let displayWeather;
+  if (cod === "404") {
+    displayWeather = errorMessage404;
+  } else if (cod === "400") {
+    displayWeather = errorMessage400;
+  } else {
+    displayWeather = <Weather props={weatherInfo} />;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Look up the weather in your city!</h1>
       </header>
+      <main>
+        <div>
+          <div>
+            <input
+              className="weather-input"
+              placeholder="Enter your city name here"
+              onChange={cityInput}
+              required
+            />
+            <button className="weather-submit" onClick={getWeather}>
+              Submit
+            </button>
+          </div>
+          <div className="display-weather">{displayWeather}</div>
+        </div>
+      </main>
+      <footer>Copyright ©2020 All rights reserved</footer>
     </div>
   );
 }
