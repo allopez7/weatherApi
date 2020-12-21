0a8c741d25030580aaf7b2de2e45f65025beb9b3
Alberto Lopez <losdodgerz@gmail.com>
2020-12-20 16:58:37 -0800

    added degree symbol to fahrenheit

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -9,9 +9,9 @@ return (
     <div>
           <div>{image}</div>
           <h3>{name}{comma} {sys&&sys.country}</h3>
-          <h3>{ main ? `Temp ${Math.round(main.temp)} F` : ''}</h3>
+          <h3>{ main ? `Temp ${Math.round(main.temp)} °F` : ''}</h3>
           <h3>{ weather ? weather[0].description : ''}</h3>
-          <h3>{ main ? `Feels like ${Math.round(main.feels_like)} F` : ''}</h3>
+          <h3>{ main ? `Feels like ${Math.round(main.feels_like)} °F` : ''}</h3>
           <h3>{ main ? `Humidity ${main.humidity}%` : ''}</h3>
           <h3>{ wind ? `${Math.round(wind.speed)} mph` : ''}</h3>
           </div>
